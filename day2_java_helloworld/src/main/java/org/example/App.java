45644840c024662346eef7524b985a3655fd6d50
Sathya009 <sathyapriyamcet@gmail.com>
2020-07-11 18:51:08 +0530

    my first debug test

--- day2_java_helloworld/src/main/java/org/example/App.java
+++ day2_java_helloworld/src/main/java/org/example/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        int k=1;
+        int j=2;
+        int total;
         System.out.println( "Hello World! Sakthi" );
+        System.out.println("after breakpoint");
+        total = k+j;
+        System.out.println(total);
     }
 }
